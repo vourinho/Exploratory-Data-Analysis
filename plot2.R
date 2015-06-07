4ddb04d75df9f84053f02525b28edfcb2fce71d0
Vouris Angelos <vourinho@hotmail.com>
2015-06-07 09:37:38 +0300

    plot2
    Global Active Power

---
+++ plot2.R
@@ -0,0 +1,19 @@
+#Get full dataset#
+data_full <- read.csv("C:/Users/vouri_000/Desktop/coursera/4-Exploratory Data Analysis/household_power_consumption.txt", header=T,
+sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+#Subset the data#
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+#Convert dates#
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+#Plot 2#
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+#Saving to file#
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
