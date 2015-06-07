f0c86bea0068db3b3e4a26878d47d93e2834c3b2
Vouris Angelos <vourinho@hotmail.com>
2015-06-07 09:44:37 +0300

    course project 1
    Data Science

---
+++ Course Project 1/plot3.R
@@ -0,0 +1,26 @@
+#Get full dataset#
+data_full <- read.csv("C:/Users/vouri_000/Desktop/coursera/4-Exploratory Data Analysis/household_power_consumption.txt",
+header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+#Subset the data#
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+#Convert dates#
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+#Plot 3#
+with(data, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#Saving to file#
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
